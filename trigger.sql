642095b88b1965bb612f93a23464f1413de136ac
mandrantofit <mandrantofit@gmail.com>
2024-09-06 13:29:39 +0300

    06 septembre 2024 13:29

---
+++ trigger.sql
@@ -0,0 +1,23 @@
+DELIMITER //
+
+CREATE TRIGGER historique_after_delete
+AFTER DELETE ON affectation
+FOR EACH ROW
+BEGIN
+  INSERT INTO historique_affectation (
+    ID_affectation,
+    ID_utilisateur,
+    ID_materiel,
+    date_affectation
+  )
+  VALUES (
+    OLD.ID_affectation,
+    OLD.ID_utilisateur,
+    OLD.ID_materiel,
+    OLD.date_affectation
+  );
+END;
+
+//
+
+DELIMITER ;
